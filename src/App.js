558de14b01885c038f379fc5878a6dd4da417fa5
George Jones <george@circleaudiovisual.com>
2020-06-29 15:20:13 +0100

    added default fallback to porfolio page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Redirect from './Redirect'
 
+const portfolioUrl = 'http://www.georgekeesjones.com'
+
 const routes = [
   {
     path: 'celeb',
     url: 'https://find-your-celebrity-lookalike.netlify.app'
-  },  
+  },
   {
     path: 'celeb-repo',
     url: 'https://github.com/Jompra/sei-project-2'
@@ -40,10 +42,13 @@ const routes = [
 const App = () => (
   <BrowserRouter>
     <Switch>
+      <Route exact path='/' component={() => (
+        window.location.href = portfolioUrl
+      )} />
       {routes.map(route => (
-        <Route 
-          key={route.path} 
-          path={`/${route.path}`} 
+        <Route
+          key={route.path}
+          path={`/${route.path}`}
           render={(routeProps) => (
             <Redirect {...routeProps} {...route} />
           )} />
